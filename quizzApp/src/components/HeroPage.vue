746700b5bad34d5c235595ff97798a47143ea954
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-04-29 22:24:21 +0300

    Update HeroPage.vue

--- quizzApp/src/components/HeroPage.vue
+++ quizzApp/src/components/HeroPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="bg-cover bg-center ... min-h-screen" style="background-image: url('https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1005300/66cdbdfdd198c9d931868f5c456b6750e9e3f0c1.jpg')">
 <!--    <div class="py-96"></div>-->
+    <div class="text-center">
+      <p class="pt-72 lg:text-8xl sm:text-6xl leading-8 font-extrabold tracking-tight text-white">Welcome to Quizhut</p>
+    </div>
   </div>
   <router-view />
 </template>
